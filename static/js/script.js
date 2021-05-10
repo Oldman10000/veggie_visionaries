02e70129ef5d19dd7f418d0995143ce20cb2c0f6
Oldman10000 <patrickoldman1993@gmail.com>
2021-05-10 15:22:36 +0000

    restyle add/edit recipe pages

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
     // Dynamic input function taken and adapted from https://www.codexworld.com/add-remove-input-fields-dynamically-using-jquery/
-    const maxFieldIngredients = 15; //Input fields increment limitation
+    const maxFieldIngredients = 20; //Input fields increment limitation
     const maxFieldInstructions = 10; //Input fields increment limitation
     let ingredientInputs = $(".ingredient").length; //Initial field counter is amount of nodes
     let instructionInputs = $(".instruction").length; //Initial field counter is amount of nodes
@@ -12,7 +12,7 @@ $(document).ready(function () {
         if (ingredientInputs < maxFieldIngredients) {
             ingredientInputs++; //Increment field counter
             $('.ingredient_field_wrapper').append(`
-            <div class="form-outline form-white mb-4">
+            <div class="form-outline mb-4">
                 <input type="text" name="ingredient" id="ingredient${ingredientInputs}" class="form-control ingredient" required />
                 <label class="form-label" for="ingredient${ingredientInputs}">Ingredient</label>
             </div>
@@ -30,7 +30,7 @@ $(document).ready(function () {
         if (instructionInputs < maxFieldInstructions) {
             instructionInputs++; //Increment field counter
             $('.instruction_field_wrapper').append(`
-            <div class="form-outline form-white mb-4">
+            <div class="form-outline mb-4">
                 <textarea name="instruction" id="instruction${instructionInputs}" class="form-control instruction" rows=4 required></textarea>
                 <label class="form-label" for="instruction${instructionInputs}">Instruction ${instructionInputs}</label>
             </div>
@@ -45,14 +45,14 @@ $(document).ready(function () {
     //Once remove ingredient button is clicked
     $(".delete_ingredient_button").click(function (e) {
         e.preventDefault();
-        $(this).prev().children().last().remove(); //Remove field html
+        $(this).parent().prev().children().last().remove(); //Remove field html
         ingredientInputs = $(".ingredient").length;
     });
 
     //Once remove instruciton button is clicked
     $(".delete_instruction_button").click(function (e) {
         e.preventDefault();
-        $(this).prev().children().last().remove(); //Remove field html
+        $(this).parent().prev().children().last().remove(); //Remove field html
         instructionInputs = $(".instruction").length;
     });
 
@@ -68,7 +68,11 @@ $(document).ready(function () {
             if (!error && result && result.event === "success") {
                 console.log(result.info.secure_url);
                 $("#image_submit").val(result.info.secure_url);
+                $(".image-submit-div").append(`
+                    <p><i class="fas fa-check-circle"></i> ${result.info.secure_url}</p>
+                `)
+                $(".photo-widget").remove();
             }
-        })
+        });
     });
 });
