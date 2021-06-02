ef0974bafdfc9ac4fc7185da111b043eb9044414
Oldman10000 <patrickoldman1993@gmail.com>
2021-06-02 08:47:43 +0000

    Set debug to environ variable

--- app.py
+++ app.py
@@ -579,4 +579,4 @@ def not_found(e):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=os.environ.get("DEBUG"))
